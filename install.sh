0a4d55165288e3f4b7e1b9b9d845e1d124a9a590
Archie Hilton (thytom) <archie.hilton1@gmail.com>
2019-10-21 20:28:26 +0100

    Added basic install script and updated TODO.org

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CONFIG_DIR="/home/$USER/.config/dwmbar"
+MODULES_DIR="$CONFIG_DIR/modules"
+DWMBARRC="$CONFIG_DIR/dwmbarrc"
+DWMBAR="/usr/bin/dwmbar"
+
+if [[ ! -f "dwmbar" ]]; then
+	echo "dwmbar executable not found."
+fi
+
+install()
+{
+	cp "./dwmbar /usr/bin/dwmbar"
+
+	mkdir -p "$CONFIG_DIR"
+
+	[[ ! -f "$MODULES_DIR" ]] && cp  -r "./modules" "$CONFIG_DIR/modules"
+	[[ ! -f "$DWMBARRC" ]] && cp "./dwmbarrc" "$DWMBARRC"
+}
